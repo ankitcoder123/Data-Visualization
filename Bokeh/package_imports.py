daf79ae9f46b9a1010dcea82565acf116aafc84e
Ankit Kamboj <96250431+ankitcoder123@users.noreply.github.com>
2021-12-18 13:43:44 +0530

    Create package_imports.py

---
+++ Bokeh/package_imports.py
@@ -0,0 +1,11 @@
+#importing Bokeh
+from bokeh.plotting import figure
+from bokeh.io import output_file, show
+import bokeh
+import pandas as pd
+print(bokeh.__version__)
+print(pd.__version__)
+from bokeh.resources import INLINE
+import bokeh.io
+from bokeh import *
+import warnings
